280be4a25756312560c3a25e99bf9e2e936209f3
ThibaultMontois <thibault.montois@gmail.com>
2022-01-21 16:58:46 +0100

    feat(fizzbuzz++): update fizzbuzz.js, fizzbuzz_simple.js and fizzbuzz++.js

--- scripts/fizzbuzz.js
+++ scripts/fizzbuzz.js
@@ -21,16 +21,14 @@ function isMultiple(num, den) {
 
 /**
  * Check if 'num' are multiples of 3, 5 or both using 'test_function'.
- * Write respectively 'Fizz', 'Buzz' or 'FizzBuzz' in 'container' if they are.
+ * Write respectively 'Fizz', 'Buzz' or 'FizzBuzz' in a 'container' if they are.
  * 
- * @param { HTMLElement } container 
+ * @param { HTMLElement } container
  * @param { Number } num 
  * @param { Function } test_function 
  */
-function addFizzBuzz(container, num, test_function) {
-        let p = document.createElement('p');
-        p.innerText += num < 10 ? `00${num} : ` : num < 100 ? `0${num} : ` : `${num} : `; // add '00' if num < 10, '0' if num < 100
-        if (test_function(num, 3)) writeResp(p, 'Fizz');
-        if (test_function(num, 5)) writeResp(p, 'Buzz');
-        container.appendChild(p);
+function writeFizzBuzz(container, num, test_function) {
+        container.innerText += num < 10 ? `00${num} : ` : num < 100 ? `0${num} : ` : `${num} : `;
+        if (test_function(num, 3)) writeResp(container, 'Fizz');
+        if (test_function(num, 5)) writeResp(container, 'Buzz');
 }
